7c4980637a5533e6493446350bfb185f68bddea2
danbohdan <81313913+danxczm@users.noreply.github.com>
2022-09-25 23:27:42 +0300

    vimeoPlayer - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(
+    ({ seconds }) => localStorage.setItem('videoplayer-current-time', seconds),
+    1000
+  )
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
