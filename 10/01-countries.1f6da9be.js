112a85bc43bad3fb597a025f1cf99f95c977be01
danxczm <81313913+danxczm@users.noreply.github.com>
2022-10-18 07:48:26 +0000

    Deploying to gh-pages from @ danxczm/HW-08---11@64ab052fd24f0bfbb7f045997729f14cc08b8572 🚀

---
+++ 10/01-countries.1f6da9be.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/ukraine").then((function(n){return n.json()})).then((function(n){return console.log(n)}));
+//# sourceMappingURL=01-countries.1f6da9be.js.map
---
+++ 10/01-countries.887bee49.js
@@ -0,0 +1,2 @@
+fetch("https://restcountries.com/v3.1/name/ukraine").then((e=>e.json())).then((e=>console.log(e)));
+//# sourceMappingURL=01-countries.887bee49.js.map
